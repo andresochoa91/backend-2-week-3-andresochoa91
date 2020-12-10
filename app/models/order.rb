87026c7718eb8524b4d549514695744af4c89611
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-10 14:54:38 -0800

    Almost ready, only missing Test

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  validates_presence_of :customer
+
+  validates :product_name, presence: true
+  validates :product_count, presence: true
+  validates :product_count, numericality: { only_integer: true }
 end

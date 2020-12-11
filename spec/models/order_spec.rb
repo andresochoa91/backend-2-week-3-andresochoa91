c9683e273b2a03b9c43a14a7055c00ba155806ca
andresochoa91 <andres_ochoa91@hotmail.com>
2020-12-10 17:36:36 -0800

    Assignment ready

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,25 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
 
-  # Customer.create(
-  #   first_name: "yay",
-  #   last_name: "yay",
-  #   phone: "1234560789",
-  #   email: "hello@gmail.com"
-  # ) 
-
-
   subject { 
     Order.new(
       product_name: "Chocolate", 
-      product_count: 2
-      # customer_id: 1
+      product_count: 2,
+      customer_id: Customer.last.id
     ) 
   }
 
-  # it "is valid with valid attributes" do
-  #   expect(subject).to be_valid
-  # end
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
 
   it "is not valid without a product_name" do
     subject.product_name=nil
@@ -39,11 +31,6 @@ RSpec.describe Order, type: :model do
 
   it "is not valid when count is less than 1" do
     subject.product_count=0
-    expect(subject).to_not be_valid
-  end
-
-  it "is not valid when count is not integer" do
-    subject.product_count="1"
     expect(subject).to_not be_valid
   end
 
